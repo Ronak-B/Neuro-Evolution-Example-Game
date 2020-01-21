203d6ecb156af58108b912455d23a785c231d2ab
Ronak bandwal <39388672+Ronak-B@users.noreply.github.com>
2020-01-21 18:38:28 +0530

    comments

--- game.py
+++ game.py
@@ -1,10 +1,14 @@
 import pygame, sys ,os,neat,pickle
 from pygame.locals import *
 
-WIDTH=800
+#pygame window size
+WIDTH=800    
 HEIGHT=800
+
+#generation number
 gen=0
 
+#Level class to draw the level
 class Level():
     def __init__(self):
         self.blocks=[100,300,500,700]
@@ -17,11 +21,12 @@ class Level():
         for r in self.rects:
             pygame.draw.rect(display,(0,0,0),r)
 
+#Player class that is controlled by the AI
 class Blob():
     def __init__(self):
         self.x=30
         self.y=450
-        self.passed=0
+        self.passed=0      
         self.last_passed=0
         self.prev_dir=1
     
@@ -41,12 +46,14 @@ class Blob():
         if dir==4:
             self.y+=5
 
+#init pygame
 pygame.init()
 display=pygame.display.set_mode((WIDTH,HEIGHT))
 
 level=Level()
 blob=Blob()
 
+#function to evaluate the genomes
 def eval_genomes(genomes,config):
 
     global gen
@@ -56,6 +63,7 @@ def eval_genomes(genomes,config):
     nets=[]
     ge=[]
 
+    #init genomes,nets and blobs
     for genome_id,genome in genomes:
         genome.fitness=0
         net=neat.nn.FeedForwardNetwork.create(genome,config)
@@ -63,33 +71,41 @@ def eval_genomes(genomes,config):
         nets.append(net)
         ge.append(genome)
     
-
+    #run loop until all blobs die or game_over =True
     while len(blobs)>0 and game_over==False:
         for e in pygame.event.get():
             if e.type==QUIT:
                 pygame.quit()
                 exit()
 
+
         for x,blob in enumerate(blobs):
-            ge[x].fitness-=0.1
+            ge[x].fitness-=0.1    #decrease fitness of each blob for each frame it stays alive to avoid loops at one place
+
+            #calcutate no of obstacles passed
             passed=0
             for b in level.blocks:
                 if blob.x>b:
                     passed+=1
             blob.passed=passed
-            #print(passed)
+
+            #give fitness for passing obstacles
             if blob.passed>0 and blob.passed-blob.last_passed>0:
                 ge[x].fitness+=(blob.passed)*50
                 blob.last_passed=blob.passed
-            #print(ge[x].fitness)
+            
+            #calculate next open window for input to the network
             if passed<4:
                 window=level.window[passed]
                 window_top=window[0]
                 window_bottom=window[1]
             else:
-                game_over=True
+                game_over=True #id all obstacles passed then game_over=True
+
+            #get output from the neural network which decides where to move the blob
             output=nets[blobs.index(blob)].activate(((blob.y-window_bottom),(blob.y-window_top),blob.y,blob.x))
 
+            #decide the output and move the blob accordingly
             m=max(output)
 
             if output[0]==m:
@@ -123,40 +139,40 @@ def eval_genomes(genomes,config):
                     blob.move(2)
                     blob.prev_dir=2
 
-
+            #remove the blob if it is looping around 
             if ge[x].fitness<-100:
                 nets.pop(blobs.index(blob))
                 ge.pop(blobs.index(blob))
                 blobs.pop(blobs.index(blob))       
 
-
+        #detect collisions
         for r in level.rects:
             for blob in blobs:
                 if r.colliderect(pygame.rect.Rect(blob.x,blob.y,20,20)):
-                    #game over
-                    ge[blobs.index(blob)].fitness-=40
-                    nets.pop(blobs.index(blob))
-                    ge.pop(blobs.index(blob))
-                    blobs.pop(blobs.index(blob))
-                
-                if blob.x<10 or blob.y<10 or blob.y>770:
+                    #remove blob and decrease fitness
                     ge[blobs.index(blob)].fitness-=40
                     nets.pop(blobs.index(blob))
                     ge.pop(blobs.index(blob))
                     blobs.pop(blobs.index(blob))
 
-        
+                if blob.x<10 or blob.y<10 or blob.y>770:
+                    #remove blob and decrease fitness
+                    ge[blobs.index(blob)].fitness-=40
+                    nets.pop(blobs.index(blob))
+                    ge.pop(blobs.index(blob))
+                    blobs.pop(blobs.index(blob))
+
+        #update the display 
         display.fill([255,255,255])
         level.draw(display)
-        for blob in blobs:
-            #print(blob.x,blob.y)          
+        for blob in blobs:      
             blob.draw(display)
         pygame.display.flip()
 
 
 def run(config_file):
     """
-    runs the NEAT algorithm to train a neural network to play flappy bird.
+    runs the NEAT algorithm to train a neural network.
     :param config_file: location of config file
     :return: None
     """
@@ -173,11 +189,13 @@ def run(config_file):
     p.add_reporter(stats)
     #p.add_reporter(neat.Checkpointer(5))
 
-    # Run for up to 50 generations.
+    # Run for up to 200 generations.
     winner = p.run(eval_genomes,200)
-    print(type(winner))
+    
+    #store the best genome
     with open('winner.pickle','wb') as f:
         pickle.dump(winner,f)
+
     # show final stats
     print('\nBest genome:\n{!s}'.format(winner))
 
